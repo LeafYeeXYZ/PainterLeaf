770e719cdbf82ecaf4f9c2544e98eb477ad17001
LeafYeeXYZ <leafyeexyz@outlook.com>
2024-03-28 19:15:52 +0800

    2.0.0

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-赛博画师小叶子
+# 赛博画师小叶子
+一个基于 `Cloudflare Workers AI` 的图片生成网站，后端见[这个项目](https://github.com/LeafYeeXYZ/PainterLeafServer)
+
+## 使用
+### 修改 `src/main.js` 的第二行
+```javascript
+const server = "https://your.server.domain" // 你的服务器地址
+```
+
+### 安装依赖
+```bash
+npm i
+```
+
+### 运行和打包
+```bash
+# 本地运行
+npm start
+# 打包
+npm run build
+```
---
+++ src/main.js
@@ -0,0 +1,79 @@
+// 定义服务端地址
+const server = 'https://painter.leafyee.xyz'
+// 获取元素
+const textarea = document.querySelector('#prompt')
+const submit = document.querySelector('#submit')
+const imgContainer = document.querySelector('.swiper-wrapper')
+const dialog = document.querySelector('dialog')
+const container = document.querySelector('.container')
+
+// 提交函数
+async function generateImage() {
+  try {
+    // 禁用按钮
+    submit.disabled = true
+    submit.textContent = '生成中...'
+    // 获取输入的文本
+    const text = textarea.value
+    // 如果没有输入文本，不发送请求
+    if (!text) throw '请输入文本'
+    // 编码为 URL
+    const encodedText = encodeURI(text)
+    // 发送请求
+    const res = await fetch(`${server}/?prompt=${encodedText}`)
+    // 响应头为 'content-type': 'image/png'
+    const blob = await res.blob()
+    // 创建一个 URL 对象
+    const imgUrl = URL.createObjectURL(blob)
+    // 创建一个图片元素
+    const img = document.createElement('img')
+    // 设置图片的 src
+    img.src = imgUrl
+    // 设置图片的 类名
+    img.className = 'swiper-slide'
+    // 添加到 imgContainer（插入到开头）
+    imgContainer.prepend(img)
+    // 更新 swiper
+    swiper.update()
+    // 恢复按钮
+    submit.disabled = false
+    submit.textContent = '生成'
+  } catch (err) {
+    if (retry) {
+      // 设置弹窗内容
+      dialog.style.setProperty('--errorContent', `"${err.message || err}"`)
+      // 打开弹窗
+      dialog.show()
+      // 恢复按钮
+      submit.disabled = false
+      submit.textContent = '生成'
+    } else {
+      retry = true
+      // 重新生成图片
+      generateImage()
+    }
+  }
+}
+
+// 监听点击事件
+let retry = false
+submit.addEventListener('click', e => {
+  e.preventDefault()
+  retry = false
+  generateImage()
+})
+
+// 关闭弹窗的函数
+document.querySelector('.noticeButton').addEventListener('click', () => {
+  // 关闭弹窗
+  dialog.close()
+  // 清除弹窗内容
+  dialog.style.setProperty('--errorContent', '')
+})
+
+
+
+
+
+
+
