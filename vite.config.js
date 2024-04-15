d2214d5e3eae205f7b6e6ae298fb8f1ead7d1241
LeafYeeXYZ <leafyeexyz@outlook.com>
2024-04-15 10:42:01 +0800

    并没有解决

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    target: ['esnext'],
+    target: ['chrome89', 'firefox89', 'safari15', 'edge89'],
     rollupOptions: {
       output: {
         manualChunks: {
