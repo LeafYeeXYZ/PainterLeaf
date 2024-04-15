4b35879edd1b7666eb428992bcb397a0a2e4a9a2
LeafYeeXYZ <leafyeexyz@outlook.com>
2024-04-15 16:14:22 +0800

    up

--- src/libs/clearDB.js
+++ src/libs/clearDB.js
@@ -16,7 +16,7 @@ export default async function clearDB(targetVersion) {
       await clear()
       localStorage.setItem('dbVersion', targetVersion)
       localStorage.setItem('dbStatu', 'cleared')
-      return { type: 'open', title: '提示', content: '网站数据已更新，旧数据库已清空, 收藏的图片将在下次刷新时消失; 如果需要保留, 请在刷新前手动取消收藏后, 重新收藏图片' }
+      return { type: 'open', title: '提示', content: '网站数据已更新，旧数据库已清空, 请再次刷新网页; 之前收藏的图片将在下次刷新时消失, 您可以在刷新前保存它们' }
     } else {
       localStorage.setItem('dbStatu', 'readyToClear')
       return { type: 'open', title: '提示', content: '网站数据需要更新, 请保存重要图片，并刷新网页' }
