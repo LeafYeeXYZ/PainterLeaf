0d7cad01a3d47e2afb4505a627d09db5f99b5e63
LeafYeeXYZ <leafyeexyz@outlook.com>
2024-04-24 18:17:15 +0800

    up

--- src/libs/check.js
+++ src/libs/check.js
@@ -39,7 +39,7 @@ export default async function clearDB(targetVersion) {
   // 获取当前版本号
   const thisVersion = Number(localStorage.getItem('dbVersion')) || 0
   // 如果当前版本号不同于目标版本号则清空 IndexedDB
-  if (thisVersion !== targetVersion) {
+  if (thisVersion !== targetVersion && thisVersion !== 0) {
     const dbStatu = localStorage.getItem('dbStatu')
     if (dbStatu === 'readyToClear') {
       await clear()
@@ -50,6 +50,8 @@ export default async function clearDB(targetVersion) {
       localStorage.setItem('dbStatu', 'readyToClear')
       return { type: 'open', title: '提示', content: '网站数据需要更新, 请保存重要图片，并刷新网页' }
     }
+  } else if (thisVersion === 0) {
+    localStorage.setItem('dbVersion', targetVersion)
   }
   return
 }
