307d09f515a99c210b1cda6dfae9e0629b9d4307
LeafYeeXYZ <leafyeexyz@outlook.com>
2024-05-16 14:28:22 +0800

    up

--- vite.config.ts
+++ vite.config.ts
@@ -7,12 +7,5 @@ export default defineConfig({
   build: {
     // 修改编译目标后记得同步修改 check.ts 中的版本号
     target: ['chrome108', 'edge108', 'firefox101', 'safari15.4'],
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          antd: ['antd', '@ant-design/icons'],
-        },
-      },
-    },
   },
 })
