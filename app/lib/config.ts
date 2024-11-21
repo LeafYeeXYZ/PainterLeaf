de31a52767485c01bce2eab281a36fd230f91ba5
LeafYeeXYZ <leafyeexyz@outlook.com>
2024-11-21 19:50:37 +0800

    chore: update model list

--- app/lib/config.ts
+++ app/lib/config.ts
@@ -9,12 +9,13 @@ export type Model = {
 export const Models: Model[] = [
   // 由于这个模型和 Cloudflare 的其他模型返回值不同, 故在 image/route.ts 中进行了两处特殊处理
   { value: '@cf/black-forest-labs/flux-1-schnell', label: '☁️ FLUX.1 Schnell' },
-  { value: '@cf/stabilityai/stable-diffusion-xl-base-1.0', label: '☁️ SDXL Base 1.0' },
+  { value: '@cf/stabilityai/stable-diffusion-xl-base-1.0', label: '☁️ SDXL Base' },
   { value: '@cf/bytedance/stable-diffusion-xl-lightning', label: '☁️ SDXL Lightning' },
   { value: '@hf/black-forest-labs/FLUX.1-dev', label: '🤗 FLUX.1 Dev' },
   { value: '@hf/stabilityai/stable-diffusion-3.5-large', label: '🤗 SD 3.5 Large' },
   { value: '@hf/stabilityai/stable-diffusion-3.5-large-turbo', label: '🤗 SD 3.5 Large Turbo' },
   { value: '@hf/Shakker-Labs/SD3.5-LoRA-Chinese-Line-Art', label: '🤗 SD 3.5 Chinese Line Art' },
+  { value: '@hf/John6666/mala-anime-mix-nsfw-pony-xl-v5new-sdxl-spo', label: '🤗 SDXL NSFW' },
 ]
 
 export const ANTD_THEME_LIGHT: ThemeConfig = {
